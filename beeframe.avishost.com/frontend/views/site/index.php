9bf7ff9196787e35f11857e35e2b8484ea82c686
Kiarash Mozafari کیارش مظفری <kiamoz.gtalk@gmail.com>
2025-08-08 17:12:49 +0330

    use form

--- beeframe.avishost.com/frontend/views/site/index.php
+++ beeframe.avishost.com/frontend/views/site/index.php
@@ -2,64 +2,89 @@
 
 /** @var yii\web\View $this */
 
+use common\models\Airline;
+use common\models\AirPort;
+
 $this->title = 'My Yii Application';
 
-
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://test.api.amadeus.com/v1/security/oauth2/token',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS => 'grant_type=client_credentials&client_id=mQfAaHYglYg0q3s50celqqhSyJFUGzrv&client_secret=Eueo8LdI6VAKIOYp',
-  CURLOPT_HTTPHEADER => array(
-    'Content-Type: application/x-www-form-urlencoded'
-  ),
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);
-//echo $response;
+?>
 
 
 
-$result = json_decode($response);
-//echo $result->access_token;
+<?php $form = \yii\widgets\ActiveForm::begin(); ?>
+
+
+  <input name="origin" placeholder="origin" />
+  <input name="destination" placeholder="destination" />
+  
+  <input name="date" placeholder="date" />
+
+
+  <input type="submit" />
+
+
+<?php \yii\widgets\ActiveForm::end(); ?>
+
+<?php
+
+
+if (isset($_POST['origin']) and isset($_POST['destination']) and isset($_POST['date'])) {
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://test.api.amadeus.com/v1/security/oauth2/token',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => 'grant_type=client_credentials&client_id=mQfAaHYglYg0q3s50celqqhSyJFUGzrv&client_secret=Eueo8LdI6VAKIOYp',
+    CURLOPT_HTTPHEADER => array(
+      'Content-Type: application/x-www-form-urlencoded'
+    ),
+  ));
+
+  $response = curl_exec($curl);
+
+  curl_close($curl);
+  //echo $response;
+
+
+
+  $result = json_decode($response);
+  //echo $result->access_token;
 
 
 
 
 
-$tomorrow = date("Y-m-d", strtotime("+1 day"));
+  $tomorrow = date("Y-m-d", strtotime("+1 day"));
 
 
 
-$curl = curl_init();
+  $curl = curl_init();
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://test.api.amadeus.com/v2/shopping/flight-offers',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS =>'{
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://test.api.amadeus.com/v2/shopping/flight-offers',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => '{
   "currencyCode": "USD",
   "originDestinations": [
     {
       "id": "1",
-      "originLocationCode": "DXB",
-      "destinationLocationCode": "DOH",
+      "originLocationCode": "'.$_POST['origin'].'",
+      "destinationLocationCode": "'.$_POST['destination'].'",
       "departureDateTimeRange": {
-        "date": "'.$tomorrow.'",
+        "date": "' . $_POST['date'] . '",
         "time": "10:00:00"
       }
     }
@@ -88,120 +113,141 @@ curl_setopt_array($curl, array(
     }
   }
 }',
-  CURLOPT_HTTPHEADER => array(
-    'Content-Type: application/json',
-    'Authorization: Bearer '.$result->access_token
-  ),
-));
+    CURLOPT_HTTPHEADER => array(
+      'Content-Type: application/json',
+      'Authorization: Bearer ' . $result->access_token
+    ),
+  ));
 
-$response = curl_exec($curl);
+  $response = curl_exec($curl);
 
-curl_close($curl);
-//echo $response;
+  curl_close($curl);
+  //echo $response;
 
-$php_result = json_decode($response);
-
-
+  $php_result = json_decode($response);
 
 
 
-?>
-<div class="site-index">
+
+
+  ?>
+  <div class="site-index">
     <div class="p-5 mb-4 bg-transparent rounded-3">
-        <div class="container-fluid py-5 text-center">
-            <h1 class="display-4">Congratulations!</h1>
-            <p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
-            <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-        </div>
+      <div class="container-fluid py-5 text-center">
+        <h1 class="display-4">Congratulations!</h1>
+        <p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
+        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
+      </div>
     </div>
 
     <div class="body-content">
 
-        <div class="row">
-            
-            
-            <?php
-            foreach($php_result->data as $flight){
-                
-            
-            ?>
-            
-            <div class="col-lg-4">
-                <?php foreach($flight->itineraries[0]->segments as $segment){
-                    
-if($segment->carrierCode){
+      <div class="row">
 
-    echo "enter_1<br>";
-$curl = curl_init();
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://test.api.amadeus.com/v1/reference-data/airlines?airlineCodes='.$segment->carrierCode,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-  CURLOPT_HTTPHEADER => array(
-    'Authorization: Bearer '.$result->access_token
-  ),
-));
+        <?php
+        foreach ($php_result->data as $flight) {
 
-$response = curl_exec($curl);
 
-//echo $response;
+          ?>
 
-curl_close($curl);
-//$php_resultAirline = json_decode($response, true);
+          <div class="col-lg-4">
+            <?php foreach ($flight->itineraries[0]->segments as $segment) {
 
-$data = json_decode($response, true); 
+              if ($segment->carrierCode) {
 
-if(isset($data['data'][0])){
-$airline = $data['data'][0]['businessName'];
-}else{
-$airline = "---";
 
-}
+                //check iatacode airline exist in my db or not 
+                $fund_iata_airline = Airline::findOne(['iatacode' => $segment->carrierCode]);
+                if (!$fund_iata_airline) {
 
-// Access the business name
+
+
+                  $curl = curl_init();
+
+                  curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://test.api.amadeus.com/v1/reference-data/airlines?airlineCodes=' . $segment->carrierCode,
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'GET',
+                    CURLOPT_HTTPHEADER => array(
+                      'Authorization: Bearer ' . $result->access_token
+                    ),
+                  ));
+
+                  $response = curl_exec($curl);
+
+                  //echo $response;
+        
+                  curl_close($curl);
+                  //$php_resultAirline = json_decode($response, true);
+        
+                  $data = json_decode($response, true);
+
+                  if (isset($data['data'][0])) {
+                    $airline = $data['data'][0]['businessName'];
+
+                    $new_airline = new Airline();
+                    $new_airline->iatacode = $segment->carrierCode;
+                    $new_airline->business_name = $airline;
+                    $new_airline->save();
+
+                  } else {
+                    $airline = "---";
+
+                  }
+
+                } else {
+                  $airline = $fund_iata_airline->business_name;
+                }
+
+                // Access the business name
 //$businessName = $data['data'][0]['businessName'];
+        
 
+              }
 
-}
-
-//print_r( $php_resultAirline['data'][0]['businessName']);
+              //print_r( $php_resultAirline['data'][0]['businessName']);
 //
 //print_r($php_resultAirline->data[0]);
 //print_r($php_resultAirline->dat);
+        
 
-                
-                echo $segment->departure->iataCode."--->";
-                echo $segment->arrival->iataCode."(".$airline.")";
-                
-                    ?>
-                    <h2><?php  ?></h2>
-                    <?php
-            
-                ?>
-                
-                
-                <?php } ?>
-                
-                <p>
-                    
-                </p>
-                
-                <p><a class="btn btn-outline-secondary" >€ <?php
-echo $flight->price->total;
-?></a></p>
-                
-                
-            </div>
-            
+              echo $segment->departure->iataCode . "--->";
+              echo $segment->arrival->iataCode . "(" . $airline . ")";
+
+              ?>
+              <h2><?php ?></h2>
+              <?php
+
+              ?>
+
+
             <?php } ?>
-           
-        </div>
+
+            <p>
+
+            </p>
+
+            <p><a class="btn btn-outline-secondary">€
+                <?php
+                echo $flight->price->total;
+                ?>
+              </a></p>
+
+
+          </div>
+
+        <?php } ?>
+
+      </div>
 
     </div>
-</div>
+  </div>
+
+
+<?php } ?>
