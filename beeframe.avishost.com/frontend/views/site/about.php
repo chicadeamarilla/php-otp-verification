1bc30cd48f90391dd7e494a7003ca361ec0f3a92
Kiarash Mozafari کیارش مظفری <kiamoz.gtalk@gmail.com>
2025-08-14 19:04:43 +0330

    about hotel

--- beeframe.avishost.com/frontend/views/site/about.php
+++ beeframe.avishost.com/frontend/views/site/about.php
@@ -8,13 +8,42 @@ $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+  <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+  <p>This is the About page. You may modify the following file to customize its content:</p>
 
-    <code><?= __FILE__ ?></code>
+  <code><?= __FILE__ ?></code>
 </div>
 <?php
+
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://test.api.amadeus.com/v1/security/oauth2/token',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => 'grant_type=client_credentials&client_id=mQfAaHYglYg0q3s50celqqhSyJFUGzrv&client_secret=Eueo8LdI6VAKIOYp',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/x-www-form-urlencoded'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+//echo $response;
+
+
+
+$result = json_decode($response);
+
 
 $curl = curl_init();
 
@@ -28,15 +57,37 @@ curl_setopt_array($curl, array(
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => 'GET',
   CURLOPT_HTTPHEADER => array(
-    'Authorization: Bearer yHuuBFSTnxELrToilJLSrqsTdszh
-'
+    'Authorization: Bearer ' . $result->access_token
   ),
 ));
 
 $response = curl_exec($curl);
 
 curl_close($curl);
-echo $response;
-  $php_result = json_decode($response);
 
+$php_result = json_decode($response);
+
+
+//print_r($php_result);
+
+
+foreach ($php_result->data as $hotel) {
+
+
+  //print_r($hotel);
   
+
+  echo "name:" . $hotel->name;
+  echo "<br>";
+  echo "iata code :" . $hotel->iataCode;
+  echo "<br>";
+  if (isset($hotel->address->cityName)) {
+    echo $hotel->address->cityName;
+  } else {
+     echo "No city name";
+  } 
+    
+  echo "<hr>";
+}
+
+
