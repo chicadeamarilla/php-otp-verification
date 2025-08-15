7d7f731d5196940743fea811c126f9ad592e0f4d
chicadeamarilla <rajabisheyda15@gmail.com>
2025-08-15 22:54:50 +0330

    routing

--- beeframe.avishost.com/frontend/controllers/SiteController.php
+++ beeframe.avishost.com/frontend/controllers/SiteController.php
@@ -145,6 +145,10 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+    public function actionHotel()
+    {
+        return $this->render('hotel');
+    }
 
     /**
      * Signs user up.
---
+++ beeframe.avishost.com/frontend/views/site/hotel.php
@@ -0,0 +1,98 @@
+
+<?php
+
+
+
+use common\models\Hotel;
+use yii\helpers\Html;
+
+?>
+<?php $form = \yii\widgets\ActiveForm::begin(); ?>
+
+
+  <input name="cityCode" placeholder="Enter city iata code" />
+  <input type="submit" />
+  <?php \yii\widgets\ActiveForm::end(); ?>
+  <?php
+  if (isset($_POST['cityCode'])){
+
+    $curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://test.api.amadeus.com/v1/security/oauth2/token',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => 'grant_type=client_credentials&client_id=mQfAaHYglYg0q3s50celqqhSyJFUGzrv&client_secret=Eueo8LdI6VAKIOYp',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/x-www-form-urlencoded'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+//echo $response;
+
+
+
+
+$result = json_decode($response);
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?cityCode='.$_POST['cityCode'].'&radius=5&radiusUnit=KM&hotelSource=ALL',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'Authorization: Bearer ' . $result->access_token
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+
+$php_result = json_decode($response);
+
+
+//print_r($php_result);
+
+
+foreach ($php_result->data as $hotel) {
+
+
+  //print_r($hotel);
+  $found_hotel = Hotel::findOne(['name'=>$hotel->name]);
+  if(!$found_hotel && isset($hotel->address->cityName )){
+    echo "going to save hotel..<br>";
+    $new_hotel= new Hotel();
+    $new_hotel->name=$hotel->name;
+    $new_hotel->city_name=$hotel->address->cityName;
+    $new_hotel->save();
+    print_r($new_hotel->getErrors());
+  }
+  echo "name:" . $hotel->name;
+  echo "<br>";
+  echo "iata code :" . $hotel->iataCode;
+  echo "<br>";
+  if (isset($hotel->address->cityName)) {
+    echo $hotel->address->cityName;
+  } else {
+     echo "No city name";
+  } 
+    
+  echo "<hr>";
+}
+  }
